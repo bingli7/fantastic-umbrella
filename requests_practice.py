de91564700be933d35a756a47e707ded5c06791a
bingli <bingli@redhat.com>
2018-03-01 20:06:06 +0800

    __name__==__main__

--- requests_practice.py
+++ requests_practice.py
@@ -6,19 +6,22 @@ import requests
 '''
 Practice on 'requests'
 '''
+def test_cookies():
+	my_requests = requests.session()
 
-my_requests = requests.session()
+	# set my own cookie
+	my_requests.get("http://httpbin.org/cookies/set?bingli=redhat")
 
-# set my own cookie
-my_requests.get("http://httpbin.org/cookies/set?bingli=redhat")
+	# return 'requests.models.Response'
+	my_response = my_requests.get("http://httpbin.org/cookies")
 
-# return 'requests.models.Response'
-my_response = my_requests.get("http://httpbin.org/cookies")
+	print "type of 'my_response':  ", type(my_response)
+	print my_response.text
 
-print "type of 'my_response':  ", type(my_response)
-print my_response.text
+	my_requests.get("http://httpbin.org/cookies/delete?bingli")
+	new_response = my_requests.get("http://httpbin.org/cookies")
+	print "new cookies:  \n", new_response.text
 
 
-my_requests.get("http://httpbin.org/cookies/delete?bingli")
-new_response = my_requests.get("http://httpbin.org/cookies")
-print "new cookies:  \n", new_response.text
+if __name__ == "__main__":
+	test_cookies()
